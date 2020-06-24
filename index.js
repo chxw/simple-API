f4a4af50061f3e2fdfe3b157d1f054415bd0b34d
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-24 16:04:00 -0400

    REV: reverted changes

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ router.post('/rides', cors(corsOptions), (req, res) => {
 // Handle requests for passenger information
 router.get('/passenger.json', cors(corsOptions), check('username'), (req, res) => {
 	const errors = validationResult(req);
-	if (errors.isEmpty() && Object.keys(req.query).length !=== 0){
+	if (errors.isEmpty()){
 		var username = req.query.username
 		username = validator.escape(username)
 
