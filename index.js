36e156d58e9409ae7eba41211b74e2ccd103c075
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-24 15:19:45 -0400

    REV: new check for req.query.username

--- index.js
+++ index.js
@@ -76,18 +76,16 @@ router.post('/rides', cors(corsOptions), (req, res) => {
 
 // Handle requests for passenger information
 router.get('/passenger.json', cors(corsOptions), (req, res) => {
-	if (req.query.username !== undefined){
-		var username = req.query.username
-
-		client
-			.query('SELECT * FROM passenger WHERE username = $1', [username])
-			.then(result => res.json(result.rows))
-			.catch(e => res.send(500))
-			.then(() => client.end())
-	}
-	else{
+	if (req.query.username === {}){
 		res.json([])
 	}
+	var username = req.query.username
+
+	client
+		.query('SELECT * FROM passenger WHERE username = $1', [username])
+		.then(result => res.json(result.rows))
+		.catch(e => res.send(500))
+		.then(() => client.end())
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
