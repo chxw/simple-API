4ada3e10d2a59677cce5dc6ec0d642fcc61ce396
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-24 16:00:53 -0400

    REV: object lenght

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ router.post('/rides', cors(corsOptions), (req, res) => {
 // Handle requests for passenger information
 router.get('/passenger.json', cors(corsOptions), check('username'), (req, res) => {
 	const errors = validationResult(req);
-	if (errors.isEmpty() && req.query !== {}){
+	if (errors.isEmpty() && Object.keys(req.query).length !=== 0){
 		var username = req.query.username
 		username = validator.escape(username)
 
