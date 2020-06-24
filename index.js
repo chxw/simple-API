be6a066206d463df0184f27fb4d1e992171c143f
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-24 16:10:04 -0400

    REV: catch no query case GET /passenger.json

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ router.post('/rides', cors(corsOptions), (req, res) => {
 // Handle requests for passenger information
 router.get('/passenger.json', cors(corsOptions), check('username'), (req, res) => {
 	const errors = validationResult(req);
-	if (!errors.isEmpty()){
+	if (!errors.isEmpty() ||  Object.keys(req.query).length === 0){
 		res.json([])
 	}
 
