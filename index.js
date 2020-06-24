c68926743965505cbfd8b862a7396a78cde60927
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-24 17:01:11 -0400

    REV: missing ; in query

--- index.js
+++ index.js
@@ -81,15 +81,14 @@ router.post('/rides', cors(corsOptions), check('username'), check('lat'), check(
 router.get('/passenger.json', cors(corsOptions), check('username'), (req, res) => {
 	var errors = validationResult(req);
 	if (!errors.isEmpty() ||  Object.keys(req.query).length === 0){
-		res.send(errors)
-		// res.json([])
+		res.json([])
 	}
 
 	var username = req.query.username
 	username = validator.escape(username)
 
 	client
-		.query('SELECT * FROM passenger WHERE username = $1', [username])
+		.query('SELECT * FROM passenger WHERE username = $1;', [username])
 		.then(result => res.json(result.rows))
 		.catch(e => res.json([]))
 		.then(() => client.end())
