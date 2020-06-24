b50668edfce691676bd92d9c4e775ddd3d15e24e
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-23 23:59:46 -0400

    REV: parseFloat

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ router.post('/rides', cors(corsOptions), (req, res) => {
   	lat = validator.escape(lat)
   	lng = validator.escape(lng)
 
-  	if (req.body.username && isFloat(lat) && isFloat(lng)){
+  	if (req.body.username && isFloat(parseFloat(lat)) && isFloat(parseFloat(lng))){
   		client.query('INSERT INTO passenger (username, lat, lng) VALUES ('+username+', '+lat+', '+lng), (error, result) => {
   			if (!error){
   				res.json(data)
