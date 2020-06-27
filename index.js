61a7f4023e30001717a300726835263c5d27e55f
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-27 19:16:03 -0400

    REV: rever last change

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ router.get('/', function(req, res){
 	    if (err) {
 	      res.sendStatus(500)
 	    } else {
-	      res.render('index.ejs', { index: result })
+	      res.render('index.ejs', { passenger: result })
 	    }
 	  })
 	})
