305b6965d1053c89ec9708b3abe1fcb13cf5dd60
Chelsea Wang <chexuhuwa@gmail.com>
2020-06-27 19:33:22 -0400

    REV: ejs issue

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ router.get('/', function(req, res){
 	    if (err) {
 	      res.sendStatus(500)
 	    } else {
-	      res.render('index.ejs', { passenger: result })
+	      res.render('index.ejs', { data: result })
 	      res.end()
 	    }
 	  })
